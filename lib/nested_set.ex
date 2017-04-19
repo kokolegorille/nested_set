550d0fc2eef93efcaff0bdd938e68ac92083d5ea
koko le gorille <koko.le.gorille@gmail.com>
2017-04-19 23:31:04 +0200

    Initial commit

---
+++ lib/nested_set.ex
@@ -0,0 +1,18 @@
+defmodule NestedSet do
+  @moduledoc """
+  Documentation for NestedSet.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NestedSet.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
